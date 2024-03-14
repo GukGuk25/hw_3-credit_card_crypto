843e1d5727eaaa8779c42f8493a947f22e0c9fd9
JReyG <jansen121202@gmail.com>
2024-03-14 22:22:50 +0800

    Fix More Code Writing Style Using Rubocop
    Fix More Code Writing Style Using Rubocop

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,28 +1,21 @@
+# frozen_string_literal: true
+
 # Creating Luhn Validator Algorithm
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-  def validate_checksum
+  def validate_checksum # rubocop:disable Metrics/AbcSize
     nums_a = number.to_s.chars.map(&:to_i)
     checksum = nums_a.pop # Get the last digit to check the sum
-    # puts "\n NEW CARD"
-    # puts checksum
-    # print nums_a, "\n"
-    # reversed_num_mul = reversed then mul the even with 2
-    reversed_num_mul = nums_a.reverse.map.with_index { |m, i|
+    reversed_num_mul = nums_a.reverse.map.with_index do |m, i|
       if i.even?
-        temp = m * 2
-        temp > 9 ? temp - 9 : temp # -9 is just like adding the first and second digit source : wikipedia
+        (m * 2) > 9 ? (m * 2) - 9 : (m * 2) # -9 is just like adding the first and second digit source : wikipedia
       else
         m
       end
-    }
-    # #for debugging purposes
-    # print "checksum = ", checksum, " result = ",(10 - (reversed_num_mul.sum % 10)), "\n"
-    # print "returned value = ",  (10 - (reversed_num_mul.sum % 10)) == checksum
+    end
     ((10 - (reversed_num_mul.sum % 10)) % 10) == checksum
-    # TODO: use the integers in nums_a to validate its last check digit
   end
 end
